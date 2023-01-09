df9c439935938cb9f2ba79c8c73394f0c66a52b2
1 <flaimerofficial@yandex.ru>
2023-01-09 19:53:08 +0300

    First commit

---
+++ test.sql
@@ -0,0 +1,13 @@
+-- Database: test.db
+
+-- DROP DATABASE IF EXISTS "test.db";
+
+CREATE DATABASE "test.db"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Russian_Russia.1251'
+    LC_CTYPE = 'Russian_Russia.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
