49af2671a949377aaa3085d5349a7f29852a93ff
1 <flaimerofficial@yandex.ru>
2023-01-10 17:59:21 +0300

    Add analyzator

--- analyze/analyze.py
+++ analyze/analyze.py
@@ -2,10 +2,10 @@ import os
 import json
 pylint = os.popen('pylint ./app').read()
 if pylint:
-     raise RuntimeError('Ошибки качества кода')
+     raise print('Ошибки качества кода')
 
 os.system('bandit -iii -lll -q -r ./app/ -o bandit.json -f json ')
 with open('bandit.json', 'r') as f:
     b = json.load(f)
 if b['results']:
-    raise RuntimeError('Ошибки')
+    raise print('В коде присутсвуют ошибки')
